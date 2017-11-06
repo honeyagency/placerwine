2a493c9e512ff6840cadc9bc9ecde97192a98c26
joshre <joshre@me.com>
2017-11-06 15:53:11 -0800

    Mobile Navigation

--- static/site.js
+++ static/site.js
@@ -1,10 +1,11 @@
 jQuery(document).ready(function($) {
-	   var myLazyLoad = new LazyLoad({
+    var myLazyLoad = new LazyLoad({
         // example of options object -> see options section
         threshold: 500,
         throttle: 30,
         show_while_loading: false,
     });
+
     function toggleSearch() {
         $body = $('body');
         $search = $('.block--search-modal');
@@ -15,5 +16,25 @@ jQuery(document).ready(function($) {
     $toggle.on('click touchstart', function(event) {
         event.preventDefault();
         toggleSearch();
+    });
+    $('.trigger--children').on('click touchstart', function(event) {
+        event.preventDefault();
+        $parent = $(this).parent();
+        $child = $parent.find('.nav-drop');
+        $openChild = $('.nav-drop.open');
+        if ($parent.hasClass('children--visible')) {
+            $parent.removeClass('children--visible');
+            $child.removeClass('open').slideUp();
+        } else {
+            $('.children--visible').removeClass('children--visible');
+            $openChild.removeClass('open').slideUp();
+            $parent.addClass('children--visible');
+            $child.addClass('open');
+            $child.slideDown();
+        }
+    });
+    $('.menu--trigger').on('click touchstart', function(event) {
+        event.preventDefault();
+        $('body').toggleClass('navopen');
     });
 });
