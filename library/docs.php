8b4e14145ebe31e3667bdab442ed71d759b495e0
joshre <joshre@me.com>
2017-12-18 14:08:09 -0800

    Docs

--- library/docs.php
+++ library/docs.php
@@ -1,16 +1,20 @@
+
 <style type="text/css" media="screen">li{ padding-left: 20px;}.docitem{margin-bottom: 10px;}</style>
-<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#placer-wine-wordpress-documentation"><?php echo get_bloginfo('name'); ?> Wordpress Documentation</a></div>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#placer-wine-wordpress-documentation">Placer Wine Wordpress Documentation</a></div>
 <div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#wordpress-overview">WordPress Overview</a></div>
 <div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#logging-in">Logging In</a></div>
 <div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#media-library">Media Library</a></div>
 <ul>
 	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-a-media-item">Adding a Media Item</a></li>
 </ul>
-<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding/editing-a-page">Adding/Editing a Page</a></div>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-editing-a-page">Adding/Editing a Page</a></div>
 <ul>
 	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-a-new-page">Adding A New Page</a></li>
 	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#content">Content</a></li>
-	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#published-/-draft-/-schedulded-/-private">Published / Draft / Schedulded / Private</a></li>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#published-draft-schedulded-private">Published / Draft / Schedulded / Private</a></li>
 	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#editing-a-page">Editing a Page</a></li>
 </ul>
 <div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-featured-images">Adding Featured Images</a></div>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#updating-site-options">Updating Site Options</a></div>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-content-to-a-winery">Adding content to a winery</a></div>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-content-to-events">Adding content to events</a></div>
